e31289447cd2d37da56d9654194fe9ff5e1c46ec
mark <mark@0bacebff-10ef-0310-a86f-bc4055e020e4>
2005-10-09 11:41:50 +0000

    C++ syntax inside C code fix
    git-svn-id: https://svn.iu.hio.no/projects/cfengine-2/trunk@133 0bacebff-10ef-0310-a86f-bc4055e020e4

--- src/cfetool.c
+++ src/cfetool.c
@@ -2646,9 +2646,10 @@ void DoBatch(int dbtype)
   double val=0;
   float val1=0, val2=0, val3=0, val4=0, val5=0, val6=0, val7=0, val8=0, val9=0, val10=0;
   int i = 0, j = 0, n = 0, y = 0, k = 0, w = 0;
-  time_to_update = false;
   int timeint = -1;
   struct Average av;
+
+  time_to_update = false;
 
   Verbose("Batch mode\n");
 
